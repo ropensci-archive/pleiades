222bd5e43a412d4bc6bc88039a815e4e08c4d663
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-31 16:26:50 -0700

    added pl_search draft fxn, #1

---
+++ R/pl_search.R
@@ -0,0 +1,17 @@
+#' Search for a place, name or X.
+#'
+#' This function searches a locally cached file of their data catalog.
+#' You can update the cache if you want. See examples.
+#'
+#' @export
+#' @param query A place ID
+#' @param ... Further args passed on to \code{agrep}
+#' @examples \dontrun{
+#' pl_search(query='Rome')
+#' }
+pl_search <- function(query, ...){
+  # load data
+  # search for data
+  # return data
+  message("fxn not written yet")
+}
