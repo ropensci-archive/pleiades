0eb0a875fc98fdfb45cc9f71e8dd99698f293fc9
Scott Chamberlain <myrmecocystus@gmail.com>
2016-12-16 15:22:16 -0800

    rebuild man files with new roxygen2 version

--- R/pleiades-package.r
+++ R/pleiades-package.r
@@ -1,5 +1,10 @@
-#' pleiades
+#' \strong{pleiades}
 #'
 #' @name pleiades
+#' @import curl
+#' @importFrom R6 R6Class
+#' @name crul-package
+#' @aliases crul
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @docType package
 NULL
